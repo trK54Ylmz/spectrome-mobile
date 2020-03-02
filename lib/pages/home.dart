6c8704a8556a3f31d235961879b0ea3d6e93f539
Tarık Yılmaz <tarikyilmaz@yandex.com.tr>
2020-03-02 14:55:25 +0300

    Add home screen

---
+++ lib/pages/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class HomePage extends StatefulWidget {
+  static String tag = 'home';
+
+  HomePage() : super();
+
+  @override
+  _HomeState createState() => new _HomeState();
+}
+
+class _HomeState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
