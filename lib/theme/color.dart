ecbee0f00fdec306fd87d4d7d8a00fdd121897e6
Tarık Yılmaz <tarikyilmaz@yandex.com.tr>
2020-03-06 17:58:41 +0300

    Update sign in and sign up screens

--- lib/theme/color.dart
+++ lib/theme/color.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/cupertino.dart';
 
 class ColorConst {
+  static const white = const Color(0xffffffff);
+
+  static const transparent = const Color(0x00ffffff);
+
   static const buttonColor = const Color(0xff007bff);
+
+  static const grayColor = const Color(0xffcccccc);
 }
