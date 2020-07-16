38738230336a5a61fcac84a6fa08b0e410c54781
Tarık Yılmaz <tarikyilmaz@yandex.com.tr>
2020-07-16 15:32:10 +0300

    Update comment format

--- lib/util/storage.dart
+++ lib/util/storage.dart
@@ -3,7 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 class Storage {
   static SharedPreferences _prefs;
 
-  /// load shared preferences
+  /// Load shared preferences
   static Future<SharedPreferences> load() {
     // Get shared preferences, if present in static block
     if (_prefs != null) {
